fb0a3ee4cd6fd5a7398108150877a6a58d501cda
zharskiy <m.zharskiy@yandex.ru>
2018-10-24 02:23:08 +0300

    js suck

--- zharskiy/coursework/script/script.js
+++ zharskiy/coursework/script/script.js
@@ -1,3 +1,22 @@
 document.getElementById("menu_btn-open").addEventListener("click",function(){
     document.getElementById("overlay").classList.add("show")
 })
+document.getElementById("menu_btn-close").addEventListener("click",function(){
+    document.getElementById("overlay").classList.remove("show")
+})
+
+var open = document.getElementById("menu_btn-open")
+open.onmouseover = function(){
+    document.getElementById("hover1").classList.toggle("hover1")
+}
+open.onmouseout = function(){
+    document.getElementById("hover1").classList.remove("hover1")
+}
+
+var close = document.getElementById("menu_btn-close")
+close.onmouseover = function(){
+    document.getElementById("hover2").classList.toggle("hover2")
+}
+close.onmouseout = function(){
+    document.getElementById("hover2").classList.remove("hover2")
+}
