87dea062e743c427d91da88d780fd25fbe77ebf5
Atanasov <mephistosaa@gmail.com>
2018-11-15 01:19:28 +0300

    coursework already ready.

--- Atanasov/kursach/js/scriptjursach.js
+++ Atanasov/kursach/js/scriptjursach.js
@@ -6,3 +6,10 @@ document.getElementById("buttonOverlayClose").addEventListener("click", function
 	event.preventDefault();
 	document.getElementById("overlay").classList.remove("show")
 } )
+
+document.getElementById("showMenu").addEventListener("click", function() {
+	document.getElementById("mainBlur").classList.add("mainBlurShow")
+} )
+document.getElementById("buttonOverlayClose").addEventListener("click", function() {
+	document.getElementById("mainBlur").classList.remove("mainBlurShow")
+} )
